9a4969cb39afbe8d2a09603c2dcb7405ebf03f5b
Steve <sbannerman@angelo.edu>
2018-09-07 11:39:12 -0500

    Allow CPP_TOOL_DIR to be somewhere else

--- bin/batenv-pre.bat
+++ bin/batenv-pre.bat
@@ -1,19 +1,21 @@
 rem Capture current directory so that we can change back at the end of the script
 set CURRENT_DIR=%CD%
 
-rem http://weblogs.asp.net/whaggard/archive/2005/01/28/get-directory-path-of-an-executing-batch-file.aspx
-cd /D %~dp0
-cd ../../CppTool
-set BASE_DIR=%CD%
+rem Allow for an alternative CPP_TOOL_DIR
+if [%CPP_TOOL_DIR%]==[] (
+  echo "Setting CPP_TOOL_DIR variable to default value"
+  set CPP_TOOL_DIR=C:\Tools\CS1336.2018-Fall\CppTool
+)
 
+rem http://weblogs.asp.net/whaggard/archive/2005/01/28/get-directory-path-of-an-executing-batch-file.aspx
 cd /D %~dp0
 cd ..
 set BASE_WORK_DIR=%CD%
 
 rem Define directory variables
-set BIN_DIR=%BASE_DIR%\bin
-set LIB_DIR=%BASE_DIR%\lib
-set SRC_DIR=%BASE_DIR%\src
+set BIN_DIR=%CPP_TOOL_DIR%\bin
+set LIB_DIR=%CPP_TOOL_DIR%\lib
+set SRC_DIR=%CPP_TOOL_DIR%\src
 set WORK_ETC_DIR=%BASE_WORK_DIR%\etc
 
 rem Allow for delegation to libraries
