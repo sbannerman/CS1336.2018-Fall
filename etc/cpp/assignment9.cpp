f3d2cb23bc56b5494fdcedd14fd86c2c9c3079a1
Steve <sbannerman@angelo.edu>
2018-10-06 17:14:27 -0500

    Fix up some spacing/indentation issues.

--- etc/cpp/assignment9.cpp
+++ etc/cpp/assignment9.cpp
@@ -13,30 +13,30 @@ int main()
 	cout << endl;
 
 	double priceOfItem1 = 12.95;
-	cout << "Item 1 price:   $" << priceOfItem1 << endl;
+	cout << "Item 1:   $" << priceOfItem1 << endl;
 
 	double priceOfItem2 = 24.95;
-	cout << "Item 2 price:   $" << priceOfItem2 << endl;
+	cout << "Item 2:   $" << priceOfItem2 << endl;
 
 	double priceOfItem3 = 6.95;
-	cout << "Item 3 price:   $" << priceOfItem3 << endl;
+	cout << "Item 3:   $" << priceOfItem3 << endl;
 
 	double priceOfItem4 = 14.95;
-	cout << "Item 4 price:   $" << priceOfItem4 << endl;
+	cout << "Item 4:   $" << priceOfItem4 << endl;
 
 	double priceOfItem5 = 3.95;
-	cout << "Item 5 price:   $" << priceOfItem5 << endl;
+	cout << "Item 5:   $" << priceOfItem5 << endl;
 
-	cout << "----------------------" << endl;
+	cout << "----------------" << endl;
 
 	double subtotal = priceOfItem1 + priceOfItem2 + priceOfItem3 + priceOfItem4 + priceOfItem5;
-	cout << "Subtotal:       $" << subtotal << endl;
+	cout << "Subtotal: $" << subtotal << endl;
 
 	double tax = subtotal * TAX_RATE;
-	cout << "Tax:            $" << tax << endl;
+	cout << "Tax:      $" << tax << endl;
 
 	double total = subtotal + tax;
-	cout << "Total:          $" << total << endl;
+	cout << "Total:    $" << total << endl;
 
 	return 0;
 }
