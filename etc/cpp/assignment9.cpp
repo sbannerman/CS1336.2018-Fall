24ff6ee27c7a2d364375bf33c11e8fc166c67507
Steve <sbannerman@angelo.edu>
2018-10-06 17:09:19 -0500

    Here's how I would complete assignment 9.

---
+++ etc/cpp/assignment9.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+const double TAX_RATE = 0.06;
+
+int main()
+{
+	string name = "Steve Bannerman";
+	string className = "CS 1336";
+	string assignment = "Assignment 9";
+	cout << name << " - " << className << " - " << assignment << endl;
+	cout << endl;
+
+	double priceOfItem1 = 12.95;
+	cout << "Item 1 price:   $" << priceOfItem1 << endl;
+
+	double priceOfItem2 = 24.95;
+	cout << "Item 2 price:   $" << priceOfItem2 << endl;
+
+	double priceOfItem3 = 6.95;
+	cout << "Item 3 price:   $" << priceOfItem3 << endl;
+
+	double priceOfItem4 = 14.95;
+	cout << "Item 4 price:   $" << priceOfItem4 << endl;
+
+	double priceOfItem5 = 3.95;
+	cout << "Item 5 price:   $" << priceOfItem5 << endl;
+
+	cout << "----------------------" << endl;
+
+	double subtotal = priceOfItem1 + priceOfItem2 + priceOfItem3 + priceOfItem4 + priceOfItem5;
+	cout << "Subtotal:       $" << subtotal << endl;
+
+	double tax = subtotal * TAX_RATE;
+	cout << "Tax:            $" << tax << endl;
+
+	double total = subtotal + tax;
+	cout << "Total:          $" << total << endl;
+
+	return 0;
+}
