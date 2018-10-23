3a75977b2a5ebf4ccd450a0e64012513dc98ba86
Steve <sbannerman@angelo.edu>
2018-10-23 16:19:35 -0500

    How I would solve assignment 16.

---
+++ etc/cpp/assignment16.cpp
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Constants
+
+const string ID_LINE = "Steve Bannerman - CS 1336 - Assignment 16";
+
+const string OUTPUT_PREFIX_MAX = "Maximum of a and b is ";
+const string OUTPUT_PREFIX_MIN = "Minimum of a and b is ";
+
+const string PROMPT_A = "Enter an integer a (positive or negative): ";
+const string PROMPT_B = "Enter an integer b (positive or negative): ";
+
+int main()
+{
+    cout << ID_LINE << endl;
+    cout << endl;
+
+    int a;
+    cout << PROMPT_A;
+    cin >> a;
+    cout << endl;
+
+    int b;
+    cout << PROMPT_B;
+    cin >> b;
+    cout << endl;
+
+    if (a > b)
+    {
+        cout << OUTPUT_PREFIX_MAX << a << endl;
+        cout << endl;
+
+        cout << OUTPUT_PREFIX_MIN << b << endl;
+        cout << endl;
+    }
+    else
+    {
+        cout << OUTPUT_PREFIX_MAX << b << endl;
+        cout << endl;
+
+        cout << OUTPUT_PREFIX_MIN << a << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
